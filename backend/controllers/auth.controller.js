cab1ac982a441d22ebf1c359de57d28979d700c8
HAONANTAO <873190199@qq.com>
2024-02-15 19:18:37 +1100

    login and signup all with the salt-hash code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 import User from "../models/user.model.js";
-
+import bcrypt from "bcrypt";
 export const signup = async (req, res) => {
   try {
-    console.log("Request Body:", req.body);
-
     const { fullname, username, password, confirmPassword, gender } = req.body;
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "passwords do not match" });
@@ -18,6 +16,9 @@ export const signup = async (req, res) => {
     //continue to sign up
 
     //hash the password
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     //random avatar API
     //https://avatar-placeholder.iran.liara.run/
@@ -26,7 +27,7 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullname,
       username,
-      password,
+      password: hashedPassword,
 
       gender,
       profilePic,
@@ -47,11 +48,27 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: err.message || "Internal Server Error" });
   }
 };
-export const logout = (req, res) => {
-  console.log("logoutUser");
-  res.send("logoutUser");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "can not find the user" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    console.log(passwordMatch);
+    if (!passwordMatch) {
+      return res.status(400).json({ error: "the password is not correct" });
+    }
+    return res.status(200).json({
+      result: `you successfully log in to the account with the username   ${username}`,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "server error" });
+  }
 };
-export const login = (req, res) => {
+export const logout = (req, res) => {
   console.log("login");
   res.send("login");
 };
--- backend/database/connectToMongoDB.js
+++ backend/database/connectToMongoDB.js
@@ -5,6 +5,16 @@ const connectToMongoDB = async () => {
     //link to my DB -URL
     await mongoose.connect(process.env.MONGO_DB_URL);
     console.log("connect to database");
+
+    // 监听数据库连接事件
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+
+    // 监听连接错误事件
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+    });
   } catch (err) {
     console.log(err);
   }
