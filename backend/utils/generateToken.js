973688d749dd14e5b322f94e6c42343cce118307
HAONANTAO <873190199@qq.com>
2024-02-16 18:59:06 +1100

    moving to frontend now

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,8 +4,9 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
+  //protected method
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, //ms
+    maxAge: 15 * 24 * 60 * 60 * 1000, //ms 15days
     httpOnly: true, //prevent XSS attack cross-site script attacks
     sameSite: "strict", //CSRF attacks cross site request forgery attack
     secure: process.env.NODE_ENV !== "development",
