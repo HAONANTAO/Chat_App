66f41a37949f2333e3e8dc34901ac7f0deae38de
HAONANTAO <873190199@qq.com>
2024-02-17 15:01:03 +1100

    login ui design

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const login = () => {
-  return <div>logsssssin</div>;
+  return (
+    <div className="flex flex-col items-center justify-center mx-auto min-w-96">
+      <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg">
+        <h1 className="text-3xl font-semibold text-center text-grary-300">
+          Login
+          <span className="text-blue-500"> ChatApp</span>
+        </h1>
+        <form>
+          <div>
+            <label for="username" className="p-2 label">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="Enter Username"
+              className="w-full h-10 input input-bordered"
+            />
+          </div>
+          <div>
+            <label className="p-2 label" for="password">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter Password"
+              className="w-full h-10 input input-bordered"
+            />
+          </div>
+          <a
+            href="#"
+            className="inline mt-2 text-sm hover:underline hover:text-blue-600-block">
+            {"Don’t"} have an account?
+          </a>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default login;
