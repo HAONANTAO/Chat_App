35a179d0c8ace4d78fd3059a4443e7d007b7a687
HAONANTAO <873190199@qq.com>
2024-02-17 20:30:14 +1100

    set up 3 routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Home from "./pages/home/Home";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="flex items-center justify-center h-screen p-4 ">
-      {/* <Login></Login> */}
-      {/* <Signup></Signup> */}
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
     </div>
   );
 }
