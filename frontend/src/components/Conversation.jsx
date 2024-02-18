87776e11d97d3bf0f825d9a9d605d17912042b00
HAONANTAO <873190199@qq.com>
2024-02-18 20:38:47 +1100

    select user will differ

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import useConversation from "../zustand/useConversation";
 const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  //查看是不是被选中
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex items-center gap-2 p-2 py-1 rounded cursor-pointer hover:bg-sky-500">
+      <div
+        className={`flex items-center gap-2 p-2 py-1 rounded cursor-pointer hover:bg-sky-500 
+        ${isSelected ? "bg-sky-500" : ""}`}>
         <div className="avatar online">
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
