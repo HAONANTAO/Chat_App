c5da67a3fd25abcdc45fbadd9937c68be2e7fec9
HAONANTAO <873190199@qq.com>
2024-02-18 20:30:06 +1100

    sidebar get all user and avatar works!

--- frontend/src/components/Conversations.jsx
+++ frontend/src/components/Conversations.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Conversation from "./Conversation";
 import useGetConversations from "../hooks/useGetConversations.js";
-import  getRandomEmoji from "../"
+import { getRandomEmoji } from "../utils/emojis.js";
 const Conversations = () => {
   const [loading, conversations] = useGetConversations();
   console.log(conversations);
   return (
     <div className="flex flex-col py-2 overflow-auto">
       {/* 遍历messages */}
-      {conversations.map((c) => {
-        return <Conversation key={c._id} conversations={c} emoji={}></Conversation>;
+      {conversations.map((c, idx) => {
+        return (
+          <Conversation
+            key={c._id}
+            conversation={c}
+            emoji={getRandomEmoji()}
+            // last index no divider appear
+            lastIdx={idx === conversations.length - 1}></Conversation>
+        );
       })}
       {loading ? (
         <span className="mx-auto loading loading-spinner"></span>
