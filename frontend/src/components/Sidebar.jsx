3904e2cf83f3302f123d54532257aa260f67b7a7
HAONANTAO <873190199@qq.com>
2024-02-18 20:04:38 +1100

    useConversations hook

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 const Sidebar = () => {
+  useGetConversations();
   return (
     <div className="flex flex-col p-4 border-r border-slate-500">
       <SearchInput></SearchInput>
