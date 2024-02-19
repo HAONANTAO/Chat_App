17911bf103430ed3ccdcbc8a7948ef00828ec997
HAONANTAO <873190199@qq.com>
2024-02-19 19:47:09 +1100

    test messages display

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  //看是谁发的
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-          />
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Hi! what is up?
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
         <div className="opacity-50 chat-footer">12:46</div>
       </div>
     </div>
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+
 const Messages = () => {
   const [messages, loading] = useGetMessages();
   console.log(messages);
   return (
     <div className="flex-1 px-4 overflow-auto">
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((m, idx) => {
+          return <Message key={idx} message={m}></Message>;
+        })}
+      {loading &&
+        [...Array(3)].map((_, idx) => {
+          <MessageSkeleton key={idx}></MessageSkeleton>;
+        })}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
     </div>
   );
 };
